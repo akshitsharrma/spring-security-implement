82b94c9cf3ce40a5d44432c8cb04da70d4db405b
akshitsharrma <akshitsharma1998@gmail.com>
2024-03-13 12:38:03 +0530

    implement spring security to check auto enable basic auth

---
+++ src/main/java/com/springsecurity/app/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.springsecurity.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String checkBasicAuth() {
+        return "Basic Auth Working Fine.";
+    }
+}
